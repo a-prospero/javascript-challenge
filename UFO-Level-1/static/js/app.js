80514359c5a4df09b95c36212d65839694a78c06
april Prospero Garciagit config --global user.email april.prospero@gmail.com <april.prospero@gmail.com>
2021-01-05 10:54:35 -0700

    app.js

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,4 +1,12 @@
-// from data.js
-var tableData = data;
 
-// YOUR CODE HERE!
+// from data.js
+console.log(tableData);
+var tblColumns = ["datetime", "city", "state", "country", "shape", "duration", "comments"]
+
+var tbody = d3.select("tbody");
+
+
+function buildTable(data) {
+    tbody.html ("");
+
+}
